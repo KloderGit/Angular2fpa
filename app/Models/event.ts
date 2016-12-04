d55628d4e3a78da2038b19999d45873537becb69
KloderGit <kloder3@outlook.com>
2016-12-05 01:43:17 +0300

    небольшие правки

--- app/Models/event.ts
+++ app/Models/event.ts
@@ -3,13 +3,11 @@
 export class Event{
     Id: string;
     Date: Date;
-    Selected: boolean;
     RateType: any;
 
-    constructor (id: string, date: Date, selected: boolean, RateType: any){
+    constructor (id: string, date: Date, RateType: any){
         this.Id = id;
         this.Date = date;
-        this.Selected = selected;
         this.RateType = RateType; 
     }
 }
